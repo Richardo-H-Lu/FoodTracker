eb6fd19ac9f3c7a1eab429240f7339f1e1c8314c
Richardo-H-Lu <690251569@qq.com>
2017-03-01 16:35:08 +0800

    create

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,33 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by 鲁子浩 on 2017/2/18.
+//  Copyright © 2017年 Richardo-H-Lu. All rights reserved.
+//
+
+import UIKit
+
+class Meal{
+    
+    //MARK:Properties
+    
+    var name:String
+    var photo:UIImage?
+    var rating:Int
+    
+    //MARK:Initialization
+    init? (name:String,photo:UIImage?,rating:Int){
+        //Initialize stored properties.
+        self.name=name
+        self.photo=photo
+        self.rating=rating
+        
+        //Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating<0{
+            return nil
+        }
+    }
+}
+
+
